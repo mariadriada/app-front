9a2feebcba110df05a2a8011e2fc6432e8fb6bbe
maria <mariagiraldo4@gmail.com>
2019-03-07 07:50:49 -0500

    Added Logo component

---
+++ src/Components/Logo/Logo.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react'
+
+import './Logo.scss'
+import logoimg from '../../static/img/logo.jpg'
+
+class Logo extends Component {
+    render() {
+        return ( 
+            <div className="Logo">
+                <img src={logoimg} alt="logo"/>
+                <label>{ this.props.name }</label>
+            </div>  
+        )
+    }
+}
+
+export default Logo
